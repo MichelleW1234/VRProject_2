4b4ddf4e2fd8545a083084ab3b53fff097b05f63
tag717 <tayoon@ucsd.edu>
2026-05-01 16:27:23 -0700

    init commit

---
+++ Assets/Script/DroneManager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.XR.Hands;
+
+public class Drone : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("DRONE COLLIDED WITH ", other);
+    }
+}
