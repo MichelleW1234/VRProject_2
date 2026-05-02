711d3d935f96ac19e6dec342c1a9c77c934cdf1c
tag717 <tayoon@ucsd.edu>
2026-05-01 17:50:47 -0700

    finished setting up hand tracking input in droneManager

--- Assets/Script/DroneManager.cs
+++ Assets/Script/DroneManager.cs
@@ -1,18 +1,41 @@
 using UnityEngine;
 using UnityEngine.XR.Hands;
+using UnityEngine.SubsystemsImplementation;
+using NUnit.Framework;
+using System.Collections.Generic;
 
 public class Drone : MonoBehaviour
 {
+
+    XRHandSubsystem handSubsystem;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        var subsystems = new List<XRHandSubsystem>();
+        SubsystemManager.GetSubsystems(subsystems);
+
+        if (subsystems.Count > 0)
+        {
+            handSubsystem = subsystems[0];
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (handSubsystem == null) return; //safety check
+
+        XRHand Lefthand = handSubsystem.leftHand;
+        if (Lefthand.isTracked)
+        {
+            Debug.Log("WE GOT THE LEFT HAND WORKING!!!!!!!!!!");
+        }
+        XRHandJoint palm = Lefthand.GetJoint(XRHandJointID.Palm);
+        if (palm.TryGetPose(out Pose palmPose))
+        {
+            Debug.Log("WE GOT THE PALM POSE WORKING!!!!!!!!!!");
+        }
+
     }
 
     private void OnTriggerEnter(Collider other)
